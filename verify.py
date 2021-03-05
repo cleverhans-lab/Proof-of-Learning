99463ed9454e97adaf00c41ed8e63f9799ac1411
nick-jia <nickjia1998@gmail.com>
2021-03-04 23:21:04 -0500

    reformat verification codes

--- verify.py
+++ verify.py
@@ -54,7 +54,7 @@ def verify_all(dir, lr, batch_size, dataset, architecture, save_freq, order, thr
     return dist_list
 
 
-def verify_topq(dir, lr, batch_size, dataset, architecture, save_freq, order, threshold, q=10, half=0):
+def verify_topq(dir, lr, batch_size, dataset, architecture, save_freq, order, threshold, epochs=1, q=10, half=0):
     if not os.path.exists(dir):
         raise FileNotFoundError("Model directory not found")
     sequence = np.load(os.path.join(dir, "indices.npy"))
@@ -65,55 +65,62 @@ def verify_topq(dir, lr, batch_size, dataset, architecture, save_freq, order, th
     else:
         assert len(order) == len(threshold)
 
-    dist_list = [[] for i in range(len(order))]
+    ckpt_per_epoch = sequence.shape[0] / epochs / save_freq
+    res = []
 
-    next_model = os.path.join(dir, f"model_step_0")
-    for i in range(0, sequence.shape[0], save_freq):
-        current_model = next_model
-        if i + save_freq >= sequence.shape[0]:
-            next_model = os.path.join(dir, f"model_step_{sequence.shape[0]}")
-        else:
-            next_model = os.path.join(dir, f"model_step_{i + save_freq}")
-        res = utils.parameter_distance(current_model, next_model, order=order,
-                                       architecture=architecture, half=half)
-        for j in range(len(order)):
-            dist_list[j].append(res[j])
+    for epoch in range(epochs):
+        print(f"Verifying epoch {epoch + 1}/{epochs}")
+        start = np.round(ckpt_per_epoch * epoch).__int__()
+        end = np.round(ckpt_per_epoch * (epoch + 1)).__int__()
+        dist_list = [[] for i in range(len(order))]
+        next_model = os.path.join(dir, f"model_step_{start * save_freq}")
+        for i in range(start, end):
+            current_model = next_model
+            if (i + 1) * save_freq >= sequence.shape[0]:
+                next_model = os.path.join(dir, f"model_step_{sequence.shape[0]}")
+            else:
+                next_model = os.path.join(dir, f"model_step_{(i + 1) * save_freq}")
+            res = utils.parameter_distance(current_model, next_model, order=order,
+                                           architecture=architecture, half=half)
+            for j in range(len(order)):
+                dist_list[j].append(res[j])
 
-    dist_arr = np.array(dist_list)
-    topq_steps = np.argpartition(dist_arr, -q, axis=1)[:, -q:]
-    if len(order) > 1:
-        # union the top-q steps of all distance metrics to avoid redundant computation
-        topq_steps = reduce(np.union1d, list(topq_steps))
+        dist_arr = np.array(dist_list)
+        topq_steps = np.argpartition(dist_arr, -q, axis=1)[:, -q:]
+        if len(order) > 1:
+            # union the top-q steps of all distance metrics to avoid redundant computation
+            topq_steps = reduce(np.union1d, list(topq_steps))
 
-    dist_list = [[] for i in range(len(order))]
-    for ind in topq_steps:
-        step = int(ind * save_freq)
-        current_model = os.path.join(dir, f"model_step_{step}")
-        if step + save_freq >= sequence.shape[0]:
-            target_model = os.path.join(dir, f"model_step_{sequence.shape[0]}")
-            reproduce = train(lr, batch_size, 0, dataset, architecture, model_dir=current_model,
-                              sequence=sequence[step:], half=half)
-        else:
-            target_model = os.path.join(dir, f"model_step_{step + save_freq}")
-            reproduce = train(lr, batch_size, 0, dataset, architecture, model_dir=current_model,
-                              sequence=sequence[step:step+save_freq], half=half)
-        res = utils.parameter_distance(target_model, reproduce, order=order,
-                                       architecture=architecture, half=half)
-        for j in range(len(order)):
-            dist_list[j].append(res[j])
+        dist_list = [[] for i in range(len(order))]
+        for ind in topq_steps:
+            step = int((start + ind) * save_freq)
+            current_model = os.path.join(dir, f"model_step_{step}")
+            if step + save_freq >= sequence.shape[0]:
+                target_model = os.path.join(dir, f"model_step_{sequence.shape[0]}")
+                reproduce = train(lr, batch_size, 0, dataset, architecture, model_dir=current_model,
+                                  sequence=sequence[step:], half=half)
+            else:
+                target_model = os.path.join(dir, f"model_step_{step + save_freq}")
+                reproduce = train(lr, batch_size, 0, dataset, architecture, model_dir=current_model,
+                                  sequence=sequence[step:step+save_freq], half=half)
+            res = utils.parameter_distance(target_model, reproduce, order=order,
+                                           architecture=architecture, half=half)
+            for j in range(len(order)):
+                dist_list[j].append(res[j])
 
-    dist_list = np.array(dist_list)
-    for k in range(len(order)):
-        print(f"Distance metric: {order[k]} || threshold: {threshold[k]} || Q={q}")
-        print(f"Average top-q distance: {np.average(dist_list[k])}")
-        above_threshold = np.sum(dist_list[k] > threshold[k])
-        if above_threshold == 0:
-            print("None of the steps is above the threshold, the proof-of-learning is valid.")
-        else:
-            print(f"{above_threshold} / {dist_list[k].shape[0]} "
-                  f"({100 * np.average(dist_list[k] > threshold[k])}%)"
-                  f" of the steps are above the threshold, the proof-of-learning is invalid.")
-    return dist_list
+        dist_list = np.array(dist_list)
+        for k in range(len(order)):
+            print(f"Distance metric: {order[k]} || threshold: {threshold[k]} || Q={q}")
+            print(f"Average top-q distance: {np.average(dist_list[k])}")
+            above_threshold = np.sum(dist_list[k] > threshold[k])
+            if above_threshold == 0:
+                print("None of the steps is above the threshold, the proof-of-learning is valid.")
+            else:
+                print(f"{above_threshold} / {dist_list[k].shape[0]} "
+                      f"({100 * np.average(dist_list[k] > threshold[k])}%)"
+                      f" of the steps are above the threshold, the proof-of-learning is invalid.")
+        res.append(dist_list)
+    return res
 
 
 def verify_initialization(dir, architecture, threshold=0.01, net=None, verbose=True):
@@ -201,6 +208,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--batch-size', type=int, default=128)
     parser.add_argument('--lr', type=float, default=0.01)
+    parser.add_argument('--epochs', type=int, default=2)
     parser.add_argument('--dataset', type=str, default="CIFAR10")
     parser.add_argument('--model', type=str, default="resnet20",
                         help="models defined in model.py or any torchvision model.\n"
@@ -213,7 +221,7 @@ if __name__ == '__main__':
                         help='metric for computing distance, cos, 1, 2, or inf')
     parser.add_argument('--q', type=int, default=2, help="Set to >1 to enable top-q verification,"
                                                          "otherwise all steps will be verified.")
-    parser.add_argument('--delta', type=float, default=[1000, 10, 0.1, 0.001],
+    parser.add_argument('--delta', type=float, default=[1000, 10, 0.1, 0.01],
                         help='threshold for verification')
 
     arg = parser.parse_args()
@@ -228,7 +236,7 @@ if __name__ == '__main__':
 
     if arg.q > 0:
         verify_topq(arg.model_dir, arg.lr, arg.batch_size, arg.dataset, architecture, arg.save_freq,
-                    arg.dist, arg.delta, q=arg.q)
+                    arg.dist, arg.delta, arg.epochs, q=arg.q)
     else:
         verify_all(arg.model_dir, arg.lr, arg.batch_size, arg.dataset, architecture, arg.save_freq,
                    arg.dist, arg.delta)
